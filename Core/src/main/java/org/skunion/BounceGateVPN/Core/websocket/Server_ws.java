d43f0ed63a7f8e32c798c687e269d0af51621a22
smallru8 <30855604+smallru8@users.noreply.github.com>
2020-06-06 11:04:03 +0800

    Update to secure-2.0

---
+++ Core/src/main/java/org/skunion/BounceGateVPN/Core/websocket/Server_ws.java
@@ -0,0 +1,45 @@
+package org.skunion.BounceGateVPN.Core.websocket;
+
+import java.net.InetSocketAddress;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+public class Server_ws extends WebSocketServer{
+
+	public Server_ws(InetSocketAddress address) {
+		super(address);
+	}
+	
+	@Override
+	public void onOpen(WebSocket conn, ClientHandshake handshake) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMessage(WebSocket conn, String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onError(WebSocket conn, Exception ex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
